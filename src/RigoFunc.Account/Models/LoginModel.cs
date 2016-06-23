0f0f2a871f4af6d37e77488fbe3f45863213afcd
RigoFunc <rigofunc@outlook.com>
2016-06-23 16:01:48 +0800

    feat: Initial commit

---
+++ src/RigoFunc.Account/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿namespace RigoFunc.Account.Models {
+    public class LoginModel {
+        /// <summary>
+        /// 用户名.
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 密码.
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// 是否记住登陆.
+        /// </summary>
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ src/RigoFunc.Account/Models/SendCodeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RigoFunc.Account.Models {
+    public class SendCodeModel {
+        /// <summary>
+        /// 电话号码.
+        /// </summary>
+        public string PhoneNumber { get; set; }
+    }
+}
