a1b21e9c0fc51c75cc5af4a71fb9b4f40c959584
RigoFunc <rigofunc@outlook.com>
2016-06-24 11:07:36 +0800

    feat: check the pre-requirement

--- src/RigoFunc.Account/IServiceCollectionExtensions.cs
+++ src/RigoFunc.Account/IServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using RigoFunc.Account;
 using RigoFunc.Account.Services;
@@ -16,7 +17,27 @@ namespace Microsoft.Extensions.DependencyInjection {
         /// <param name="setupAction">An action to configure the <see cref="ApiOptions"/>.</param>
         /// <returns>The services available in the application.</returns>
         public static IServiceCollection ConfigureAccountApi<TUser>(this IServiceCollection services, Action<ApiOptions> setupAction)
-            where TUser : class, new() {
+            where TUser : class {
+            // TODO: have any better way? Such as define an IUser { string UserName { get; set; } } interface? and than with constraint where TUser : IUser
+            // check the pre-requirement
+            var constructors = typeof(TUser).GetTypeInfo().DeclaredConstructors;
+            var found = false; 
+            foreach (var ctor in constructors) {
+                var parameters = ctor.GetParameters();
+                if(parameters == null || parameters.Length != 1) {
+                    continue;
+                }
+
+                var parameter = parameters[0];
+                if(parameter.ParameterType == typeof(string) && parameter.Name.Equals("userName", StringComparison.CurrentCultureIgnoreCase)) {
+                    found = true;
+                }
+            }
+
+            if (!found) {
+                throw new NotSupportedException($"The TUser: {nameof(TUser)} must define a constructor have and only have one string parameter with name 'userName'");
+            }
+
             if (setupAction != null) {
                 services.Configure(setupAction);
             }
